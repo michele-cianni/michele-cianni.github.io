c6395e3a51b8f0882c05ffec3e1df73e4fcfdac6
Michele Cianni <cianni.michele.private@gmail.com>
2026-04-02 18:03:41 +0200

    add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://michele-cianni.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
